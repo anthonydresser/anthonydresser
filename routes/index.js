e9bdfe107e690de7f80e4762f6b695b5fe2b3e89
Anthony Dresser <smiley.yellow@gmail.com>
2015-06-15 22:10:04 -0400

    Updated projects; created visual gas site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Anthony Dresser' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'})
 });
 
 module.exports = router;
