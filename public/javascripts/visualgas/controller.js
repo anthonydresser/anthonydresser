23819768177ead8c942d6989dba8dcd677a1705a
Anthony Dresser <smiley.yellow@gmail.com>
2015-07-09 21:28:26 -0400

    refactored main site for angular routingl; refactored visualgas to user ui router rather than ngrouter

--- public/javascripts/visualgas/controller.js
+++ public/javascripts/visualgas/controller.js
@@ -2,6 +2,7 @@ var visualGas = angular.module('visualGas');
 
 visualGas.controller('navCtrl', function($http, $scope){
   $scope.logout = function(){
+    $rootScope.user = null;
     $http.post('/visualgas/logout');
   }
 });
@@ -10,14 +11,17 @@ visualGas.controller('homeCtrl', function($scope){
 
 });
 
-visualGas.controller('loginCtrl', function($location, $http, $scope){
+visualGas.controller('loginCtrl', function($http, $scope, $state, $rootScope){
   $scope.login = function(){
     $http.post('/visualgas/login', {
                                       username: $scope.email,
                                       password: $scope.password
                                    })
          .success(function(data, status){
-           if(status === 200) $location.path('/visualgas#/');
+           if(status === 200) {
+             $rootScope.user = data;
+             $state.transitionTo('account');
+           }
          })
          .error(function(data, status){
            if(status === 401) $scope.message = data;
