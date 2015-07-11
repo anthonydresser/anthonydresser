0d183fd1cbedafaa7eb77871ab2f326185502bad
Anthony Dresser <smiley.yellow@gmail.com>
2015-07-10 21:37:50 -0400

    Edited module; added entry schema; added entry post

--- routes/visualgas.js
+++ routes/visualgas.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var Entry = require('../models/entry')
 
 router.get('/', function(req, res, next){
   var user;
   if(req.user)  {user = req.user}
-  res.render('visualgas/template',
+  res.render('visualgas/templates/main',
              {
                title:'Visual Gas',
                user: user
@@ -82,7 +83,11 @@ router.post('/login', function(req, res, next){
       return res.status(401).send("User not found");
     }
 
-    res.status(200).send({ id:user['_id'], email: user.email });
+
+    req.logIn(user, function(err) {
+     if (err) { return next(err); }
+     return res.status(200).send({ id : user.email });
+   });
   })(req, res, next);
 }, function(err, req, res, next){
   res.status(500).end();
@@ -99,7 +104,10 @@ router.post('/signup', function(req, res, next){
       return res.status(401).send("Email already in use");
     }
 
-    res.status(200).send({ id:user['_id'], email: user.email });
+    req.logIn(user, function(err) {
+     if (err) { return next(err); }
+     return res.status(200).send({ id : user.email });
+   });
 
   })(req, res, next);
 }, function(err, req, res, next){
@@ -113,9 +121,50 @@ router.post('/logout', function(req, res, next){
   return res.status(200).end();
 });
 
+router.get('/templates/addentrymodal', function(req, res, next){
+  res.render('visualgas/templates/addentrymodal');
+})
+
+router.post('/addEntry', isAuth, function(req, res, next) {
+  console.log(req.body)
+  if(isEmpty(req.body.mileage) || isEmpty(req.body.gallons) || isEmpty(req.body.date)) return res.status(400).end();
+
+  var entry = new Entry();
+
+  var date = new Date(req.body.date);
+
+  entry.mileage = req.body.mileage;
+  entry.gallons = req.body.gallons;
+  entry.user = req.user['_id'];
+  entry.date = date;
+
+  entry.save(function(err){
+    if(err) res.status(500).end()
+    res.status(200).send(entry);
+  });
+})
+
 function isAuth(req, res, next) {
   if(req.isAuthenticated()) return next();
   else return res.status(401).end();
+}
+
+function isEmpty(obj) {
+
+  // null and undefined are "empty"
+  if (obj == null) return true;
+
+  // Assume if it has a length property with a non-zero value
+  // that property is correct.
+  if (obj.length > 0)    return false;
+  if (obj.length === 0)  return true;
+
+  // check to see if the obj has its own properties
+  for (var key in obj) {
+    if (hasOwnProperty.call(obj, key)) return false;
+  }
+
+  return true;
 }
 
 module.exports = router;
