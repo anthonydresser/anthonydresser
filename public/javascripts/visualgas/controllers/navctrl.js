2dd5a38be1dfebc6d0916327e9a11c2a5c4b7e32
Anthony Dresser <smiley.yellow@gmail.com>
2015-07-16 21:37:28 -0400

    restructured file system, changed code for support

---
+++ public/javascripts/visualgas/controllers/navctrl.js
@@ -0,0 +1,13 @@
+angular.module('visualGas')
+  .controller('navCtrl', function($http, $state, $scope, api){
+    $scope.logout = function(){
+      $rootScope.user = null;
+      api.post.logout
+              .success(function(data, status){
+                if(status === 200){
+                  $rootScope.user = null;
+                  $state.transitionTo('home')
+                }
+              });
+    }
+  })
