db3b8f431c4b4cd57e7b1874f60132074515bd3e
Anthony Dresser <smiley.yellow@gmail.com>
2015-07-18 00:25:23 -0400

    began implementing mailing; styled front page of website

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,9 +1,44 @@
 var mainApp = angular.module('mainApp');
 
-mainApp.controller('indexCtrl', function(){
+mainApp.controller('indexCtrl', function($scope){
+  $scope.interval = 2500;
+  $scope.slides = [{text: 'Javascript'},
+                   {text: 'C'},
+                   {text: 'C++'},
+                   {text: 'HTML'},
+                   {text: 'Angular'},
+                   {text: 'Verilog'},
+                   {text: 'Android'},
+                   {text: 'NodeJS'},
+                   {text: 'Java'},
+                   {text: 'Github'},
+                   {text: 'Arduino'},
+                   {text: 'Raspberry Pi'},
+                   {text: 'Bootstrap'}];
 
+  $(document).ready(function(){
+    console.log($(window).width());
+    if($(window).width() <= 768){
+      console.log('setting css');
+      $('#carousel').css('margin-left', '-15px');
+      $('.navbar').removeClass('transparent');
+      console.log($('#carousel').css('margin-left'));
+    } else {
+      $('#carousel').css('margin-left','0px');
+    }
+  })
+
+  $(window).resize(function(){
+    if($(window).width() <= 768){
+      $('#carousel').css('margin-left', '-15px');
+      $('.navbar').removeClass('transparent');
+    } else {
+      $('#carousel').css('margin-left', '0px');
+      $('.navbar').addClass('transparent');
+    }
+  })
 })
 
 mainApp.controller('projectsCtrl', function(){
-  
+
 })
