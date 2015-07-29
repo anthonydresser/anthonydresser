fbc149509b0b1c79625abd07d5ddc668858cc4d5
Anthony Dresser <smiley.yellow@gmail.com>
2015-07-29 18:06:36 -0400

    added tagging and searching by tags

--- models/project.js
+++ models/project.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var projectSchema = mongoose.Schema({
   name : String,
   short : String,
+  published: Date,
   body : String,
   link : String,
   image : String,
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,6 +1,6 @@
 var mainApp = angular.module('mainApp');
 
-mainApp.controller('homeCtrl', function($rootScope, $scope){
+mainApp.controller('homeCtrl', function($rootScope, $scope, $anchorScroll){
 
   $scope.interval = 2500;
   $scope.slides = [{text: 'Javascript'},
@@ -69,8 +69,18 @@ mainApp.controller('homeCtrl', function($rootScope, $scope){
   });
 })
 
-mainApp.controller('projectsCtrl', function($scope, projects){
+mainApp.controller('projectsCtrl', function($scope, projects, $anchorScroll, $location){
+  $scope.scrollTo = function(id){
+    $location.hash(id);
+    $anchorScroll();
+  }
   $scope.projects = projects.data;
+  angular.forEach($scope.projects, function(project){
+    var dateString = new Date(project.published);
+    project.dateString = dateString.toLocaleDateString();
+    project.date = new Date(project.published);
+  })
+  console.log($scope.projects);
   var max = projects.data.length - 1;
   var min = 0;
   var ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
