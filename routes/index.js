0fc7596cb58120183613904e9f7c092bc66ba352
Anthony Dresser <smiley.yellow@gmail.com>
2015-09-24 14:14:05 -0400

    added resume

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('template', { title: 'Anthony Dresser' });
 });
+router.get('/resume', function(req, res, next){
+  res.send('resources/addresser_resume.pdf');
+})
 
 router.get('/partials/:filename', function(req, res, next){
   res.render('./partials/' + req.params.filename);
